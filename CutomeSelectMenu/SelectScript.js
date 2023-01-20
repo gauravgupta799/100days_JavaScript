ca5720bb72df28ee3bba7afea60e7439b726dc6c
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-01-21 01:00:09 +0530

    cutomeSelectMenu

--- CutomeSelectMenu/SelectScript.js
+++ CutomeSelectMenu/SelectScript.js
@@ -13,29 +13,38 @@ const countries = ["India","America","New York",
 'Indonesia'
 
 ]
-
-function addCountry(){
+// Looping on the array of countries.
+function addCountry(selectedCountry){
     countries.forEach(country =>{
-        // console.log(country);
-        let li = `<li onclick="updateName(this)">${country}</li>`;
+        let isSelected = country === selectedCountry ? 'selected':"";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
         options.insertAdjacentHTML("beforeend", li);
     })
 }
 addCountry();
 
+//Upadating the select country box with value
 function updateName(selectedLi){
     // console.log(selectedLi.innerText);
-    selectBtn.firstElementChild.innerText= selectedLi.innerText;
+    searchInput.value = "";
+    addCountry(selectedLi.innerText);
+    wrapper.classList.remove('active');
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
 }
 
+// Search functionality of countries
 searchInput.addEventListener('keyup',() => {
     let arr =[];
     let searchVal = searchInput.value.toLowerCase();
+    // console.log(searchVal);
     arr = countries.filter(data=>{
         return data.toLocaleLowerCase().startsWith(searchVal);
-    })
+    }).map(country => `<li onclick="updateName(this)">${country}</li>`).join("");
+    options.innerHTML = arr ? arr : `<p>Opps! Counrty not found.</p>`
+    // console.log(arr);
 })
 
+// Toggling the li options
 selectBtn.addEventListener("click", () => {
     wrapper.classList.toggle("active");
 });
