b36827966197b33eb900c4841877506a0a8b0ba2
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-01-23 00:12:19 +0530

    refactor the code

--- TempartureConverter/script.js
+++ TempartureConverter/script.js
@@ -5,10 +5,8 @@ function celToFar(){
     let output = (parseFloat(celsius.value) * 9/5) + 32;
     console.log(output);
     fahrenheit.value = parseFloat(output.toFixed(2));
-    console.log(output);
 }
 function farToCel(){
     let output = (parseFloat(fahrenheit.value) - 32) * 5/9;
     celsius.value = parseFloat(output.toFixed(2));
-    console.log(output);
 }
