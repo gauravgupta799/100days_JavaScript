1d0b7e2c0deeb9006cf9500032d7c571bed1d0a4
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-07 00:39:37 +0530

    File Downloader

--- FileDownloader/script.js
+++ FileDownloader/script.js
@@ -1,28 +1,31 @@
-const fileInput = document.querySelector("#input"),
-downloadBtn = document.querySelector("#download-btn")
+let fileInput = document.querySelector("#input"),
+downloadBtn = document.querySelector("#download-btn");
 
-downloadBtn.addEventListener("click", () => {
+downloadBtn.addEventListener("click", (e) => {
     e.preventDefault();
     downloadBtn.innerText = "Downloading File...";
     fetchFile(fileInput.value)
-    console.log(fileInput.value);
 });
 
 function fetchFile(url){
-    fetch(url)
-    .then((response) =>response.blob())
-    .then((file) =>{
-       let temUrl =  URL.createObjectURL(file);
-       let aTag = document.createElement("a");
-       aTag.href = temUrl;
-       aTag.download = url.replace(/^.*[\\\/]/,"")
-       document.body.appendChild(aTag);
-       aTag.click();
-       aTag.remove();
-       URL.revokeObjectURL(temUrl);
-       downloadBtn.innerText = "Download File";
-    }).catch(()=>{
-        downloadBtn.innerText = "Download File";
-        alert("Failed to download file!")
-    })
+    if(url == null){
+        alert("Please paste a url")
+    }else{
+        fetch(url)
+        .then((response) => response.blob())
+        .then((file) =>{
+           let temUrl =  URL.createObjectURL(file);
+           let aTag = document.createElement("a");
+           aTag.href = temUrl;
+           aTag.download = url.replace(/^.*[\\\/]/,"")
+           document.body.appendChild(aTag);
+           aTag.click();
+           aTag.remove();
+           URL.revokeObjectURL(temUrl);
+           downloadBtn.innerText = "Download File";
+        }).catch(()=>{
+            downloadBtn.innerText = "Download File";
+            alert("Failed to download file!")
+        })
+    } 
 }
