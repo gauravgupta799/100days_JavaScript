4bdbd2b0909629b9ba332c2d637fc6a79ae45b92
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-07 23:58:54 +0530

    Draggable Slider Tabs

---
+++ DraggableSliderTabs/Tabs.js
@@ -0,0 +1,34 @@
+const tabsBox = document.querySelector(".tabs-container"),
+arrowIcons = document.querySelectorAll(".icon i");
+// tabs = document.querySelector(".tab");
+
+let isDragging = false;
+
+const handleIcons = ()=>{
+    let scrollVal = Math.round(tabsBox.scrollLeft);
+    let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
+    arrowIcons[0].style.display = scrollVal > 0 ? "flex" : "none"
+    arrowIcons[1].style.display = maxScrollableWidth > scrollVal ? "flex" : "none"
+}
+arrowIcons.forEach(icon=>{
+    icon.addEventListener("click", ()=>{
+        tabsBox.scrollLeft += icon.id === "left" ? -350 : 350;
+        setTimeout(()=> handleIcons(), 50);
+    })
+})
+
+const dragging =(e)=>{
+    if(!isDragging){
+        tabsBox.classList.add("dragging");
+        tabsBox.scrollLeft = tabsBox.scrollLeft - e.movementX;
+        handleIcons();
+    }
+}
+const dragStop = () => {
+    isDragging = false;
+    tabsBox.classList.remove("dragging");
+}
+tabsBox.addEventListener("mousedown", () => isDragging = true)
+tabsBox.addEventListener("mousemove", dragging);
+tabsBox.addEventListener("mouseup", dragStop);
+
--- README.md
+++ README.md
@@ -16,5 +16,6 @@
  <li><h2> Country Info App:  01<sup>th</sup> February, 2023</h2></li>
  <li><h2> Age Calculator App:  03<sup>th</sup> February, 2023</h2></li>
  <li><h2> File Downloader:  06<sup>th</sup> February, 2023</h2></li>
+ <li><h2> Draggable Slider Tabs:  06<sup>th</sup> February, 2023</h2></li>
 </ol>
 
